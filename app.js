f31b2c8f0c00306f520e97fd04eee16cb102e9ca
khanz1 <assistance.xavier@gmail.com>
2025-05-06 13:29:54 +0700

    feat: login

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const UserController = require("./controllers/UserController");
 
 app.use(cors());
 app.use(express.json());
@@ -9,6 +10,8 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.post("/login", UserController.login);
 
 app.listen(3000, () => {
   console.log(`Server is running on port ${PORT}`);
